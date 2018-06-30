7f506a3b056becc262c74bfea2d5fba34dab8c86
Yash Zolmajdi <yash.zolmajdi@plutora.com>
2018-06-30 10:21:24 +1000

    added angular

--- ReactJS/src/components/header/index.js
+++ ReactJS/src/components/header/index.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 import './style.css';
 
 class Header extends Component {
+
+    handleLinkClick(link) {
+        var win = window.open(link, '_blank');
+        win.focus();
+    }
+
     render() {
         return (
             <div className="header">
@@ -12,13 +18,13 @@ class Header extends Component {
                         <div>SWEET</div>
                     </div>
                     <div className="header__options-container">
-                        <div className="header__option"><i className="fab fa-react"></i></div>
-                        <div className="header__option"><i className="fab fa-angular"></i></div>
+                        <div className="header__option"><i className="fab fa-react active"></i></div>
+                        {/* <div className="header__option"><i className="fab fa-angular"></i></div>
                         <div className="header__option"><i className="fab fa-vuejs"></i></div>
-                        <div className="header__option"><i className="fab fa-html5"></i></div>
+                        <div className="header__option"><i className="fab fa-html5"></i></div> */}
                         <div className="header__option-split"></div>
-                        <div className="header__option"><i className="fab fa-github"></i></div>
-                        <div className="header__option"><i className="fab fa-linkedin-in"></i></div>
+                        <div className="header__option" onClick={this.handleLinkClick.bind(this, "https://github.com/iConic21/SWEET")} ><i className="fab fa-github"></i></div>
+                        <div className="header__option" onClick={this.handleLinkClick.bind(this, "https://www.linkedin.com/in/yasharzolmajdi")}><i className="fab fa-linkedin-in"></i></div>
                     </div>
                 </div>
             </div>
